952018439cdb8883e062f95667d1b371972b103c
Tyler Steffensen <stef0041@algonquinlive.com>
2019-01-17 12:18:27 -0500

    initilizing playground with sepsration of concern

---
+++ src/components/PlayGround/PlayGround.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hello"></div>
+  <button>Click</button>
+</template>
+
+<script src="./PlayGround.js">
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./PlayGround.css" scoped>
+</style>
