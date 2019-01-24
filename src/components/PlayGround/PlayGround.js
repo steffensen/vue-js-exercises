6d94e66983c45c7b34f93cb833864c4b50e814f4
Tyler Steffensen <stef0041@algonquinlive.com>
2019-01-24 10:13:30 -0500

    updated

--- src/components/PlayGround/PlayGround.js
+++ src/components/PlayGround/PlayGround.js
@@ -1,3 +1,18 @@
 export default {
-  name: 'PlayGround'
+  name: 'PlayGround',
+  data() {
+    return {
+      counter: 0,
+      dynamicClassName: 'someDifferentName'
+    }
+  },
+  methods: {
+    handleClick() {
+      /* eslint-disable */
+      console.log('clicked')
+    },
+    increment() {
+      this.counter++
+    }
+  }
 }
--- src/components/PlayGround/PlayGround.vue
+++ src/components/PlayGround/PlayGround.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="hello"></div>
-  <button>Click</button>
+  <div class="hello">
+    <button @click="handleClick">Click</button>
+    <!-- <div v-bind:class="ClassName">What is going on here</div> -->
+    <div>What is going on here</div>
+    <div id="example-1">
+      <button v-on:click="increment">Add 1</button>
+      <p>The button above has been clicked {{ counter }} times.</p>
+    </div>
+  </div>
 </template>
 
 <script src="./PlayGround.js">
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style src="./PlayGround.css" scoped>
 </style>
